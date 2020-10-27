a2042a6421c4eae59fd76947a48b5112a8116bb8
Boruchigor <boruchigor@gmail.com>
2020-10-27 16:00:35 +0100

    added files and directories, with some JS and A LOT OF CSS

---
+++ ref.js
@@ -0,0 +1,8 @@
+function startSpinning(){
+    var element = document.getElementById("dollarsignee");
+    element.classList.add("fa-spin");
+}
+function stopSpinning(){
+    var element = document.getElementById("dollarsignee");
+    element.classList.remove("fa-spin");
+}
